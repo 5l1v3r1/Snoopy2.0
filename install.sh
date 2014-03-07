619aba7f1600a6e3fe2fbe8eb5e64b98cbe6be3a
Kaitlin Farr <kaitmfarr@gmail.com>
2014-03-07 12:31:59 -0800

    Modifying package installation in install script

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ if [ "$?" -ne 0 ]; then echo "[!] Failed :("; exit 1; fi
 if [ "$xplico" == "yes" ]; then
 	apt-get install -y xplico 
 fi
-pip install lxml beautifulsoup requests PIL mitmproxy ipaddr publicsuffix twisted cryptacular Flask_SQLAlchemy
+sudo pip install lxml beautifulsoup requests PIL mitmproxy ipaddr publicsuffix twisted http://pypi.python.org/packages/source/c/cryptacular/cryptacular-1.3.tar.gz Flask_SQLAlchemy
 if [ "$?" -ne 0 ]; then echo "[!] Failed :("; exit 1; fi
 
 echo "[+] Done installing packages. Onward and upward!"
